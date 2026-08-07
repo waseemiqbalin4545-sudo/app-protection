b260c0e76849df5285b708770dc7543897c2f861
waseemiqbalin4545-sudo <waseemiqbal.in4545@gmail.com>
2026-08-07 16:54:55 +0500

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,31 @@
+const jwt = require("../utils/jwt");
 
+module.exports = (req, res, next) => {
+    try {
+
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader) {
+            return res.status(401).json({
+                success: false,
+                message: "Authorization token missing"
+            });
+        }
+
+        const token = authHeader.replace("Bearer ", "");
+
+        const user = jwt.verifyToken(token);
+
+        req.user = user;
+
+        next();
+
+    } catch (err) {
+
+        return res.status(401).json({
+            success: false,
+            message: "Invalid or Expired Token"
+        });
+
+    }
+};
