3cf4c4a471bd7c998db9e2e3bc75db42c0cd94c0
waseemiqbalin4545-sudo <waseemiqbal.in4545@gmail.com>
2026-08-07 16:53:26 +0500

    Update jwt.js

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,1 +1,26 @@
+const jwt = require("jsonwebtoken");
 
+const SECRET = process.env.JWT_SECRET || "gsm_aashi_super_secret";
+
+function generateToken(admin) {
+  return jwt.sign(
+    {
+      id: admin.id,
+      username: admin.username,
+      role: admin.role
+    },
+    SECRET,
+    {
+      expiresIn: "7d"
+    }
+  );
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, SECRET);
+}
+
+module.exports = {
+  generateToken,
+  verifyToken
+};
