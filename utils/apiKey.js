f0095ff1ff3b4bc21668f1dd9ee9c1ac03212187
waseemiqbalin4545-sudo <waseemiqbal.in4545@gmail.com>
2026-08-07 16:53:59 +0500

    Update apiKey.js

--- utils/apiKey.js
+++ utils/apiKey.js
@@ -1,1 +1,21 @@
+const crypto = require("crypto");
 
+function generateApiKey(length = 32) {
+    return crypto.randomBytes(length).toString("hex");
+}
+
+function generateAppSecret(length = 64) {
+    return crypto.randomBytes(length).toString("hex");
+}
+
+function validateApiKey(apiKey) {
+    if (!apiKey) return false;
+
+    return /^[a-f0-9]{64}$/i.test(apiKey);
+}
+
+module.exports = {
+    generateApiKey,
+    generateAppSecret,
+    validateApiKey
+};
