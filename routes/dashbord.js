0ab03d1cb9a70892f9a2f4bc3dbf1182e214190d
waseemiqbalin4545-sudo <waseemiqbal.in4545@gmail.com>
2026-08-07 17:00:28 +0500

    Update dashbord.js

--- routes/dashbord.js
+++ routes/dashbord.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const router = express.Router();
 
+const auth = require("../middleware/auth");
+
+router.get("/", auth, (req, res) => {
+
+    res.json({
+
+        success: true,
+
+        message: "Dashboard API Working"
+
+    });
+
+});
+
+module.exports = router;
