9deb4d0df36a00c7b7ca62279ebee2a79abf92d8
waseemiqbalin4545-sudo <waseemiqbal.in4545@gmail.com>
2026-08-07 17:40:39 +0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
+router.get("/test", async (req, res) => {
+    req.body = {
+        username: "admin",
+        password: "admin123"
+    };
 
-const authController = require("../controllers/authController");
-
-router.post("/login", authController.login);
-
-module.exports = router;
+    return authController.login(req, res);
+});
