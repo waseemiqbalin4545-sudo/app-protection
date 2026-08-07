deae19ee6ae51cabe19f592215cd0dcb28415da1
waseemiqbalin4545-sudo <waseemiqbal.in4545@gmail.com>
2026-08-07 17:36:15 +0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,3 +6,21 @@ const authController = require("../controllers/authController");
 router.post("/login", authController.login);
 
 module.exports = router;
+const express = require("express");
+const router = express.Router();
+
+const authController = require("../controllers/authController");
+
+router.post("/login", authController.login);
+
+// Temporary Test Route
+router.get("/test", (req, res) => {
+    req.body = {
+        username: "admin",
+        password: "admin123"
+    };
+
+    return authController.login(req, res);
+});
+
+module.exports = router;
