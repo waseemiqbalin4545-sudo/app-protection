8330322b9e482afbc53394f06d1e37d22f97f078
waseemiqbalin4545-sudo <waseemiqbal.in4545@gmail.com>
2026-08-08 11:21:37 +0500

    Update apps.js

--- routes/apps.js
+++ routes/apps.js
@@ -4,12 +4,29 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const appController = require("../controllers/appController");
 
+
+// Add App
 router.post("/add", auth, appController.addApp);
 
+
+// All Apps
 router.get("/all", auth, appController.getApps);
 
+
+// Single App
 router.get("/:id", auth, appController.getApp);
 
+
+// Edit App
+router.put("/:id", auth, appController.updateApp);
+
+
+// ON / OFF
+router.patch("/:id/status", auth, appController.updateStatus);
+
+
+// Delete App
 router.delete("/:id", auth, appController.deleteApp);
+
 
 module.exports = router;
